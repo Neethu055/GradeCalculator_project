c61ab9a139571d1d91fe1d4c8c919aebb3ef8612
Neethu Mariyam Sajan <neethu.mariyamsajan@gmail.com>
2026-05-23 21:23:46 +0530

    New

---
+++ js/script.js
@@ -0,0 +1,45 @@
+let sub1 = Number(prompt("Enter marks for subject1:"))
+let sub2 = Number(prompt("Enter marks for subject2:"))
+let sub3 = Number(prompt("Enter marks for subject3:"))
+let sub4 = Number(prompt("Enter marks for subject4:"))
+let sub5 = Number(prompt("Enter marks for subject5:"))
+
+
+let total = sub1 + sub2 + sub3 + sub4 + sub5
+
+let avg = total/5
+
+let grade
+
+if(avg >=90){
+    grade ="A+"
+}
+
+else if(avg>=80){
+    grade="A"
+}
+
+else if(avg>=70){
+    grade="B"
+}
+
+else if(avg>=60){
+    grade="C"
+}
+
+else if(avg>=50){
+    grade="D"
+}
+
+else{
+    grade="F"
+}
+
+alert(
+    "Total Marks: " +total+
+    "\nAverage Marks: " +avg50+
+    "\nGrade:" +grade
+)
+
+85
+
